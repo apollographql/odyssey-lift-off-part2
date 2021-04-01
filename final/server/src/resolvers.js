08ca99644b627c64fd17b4d0299d2d8755cdc646
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-04-01 16:47:47 -0400

    add final code for lift-off part 2 course

---
+++ final/server/src/resolvers.js
@@ -0,0 +1,15 @@
+const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+  },
+};
+
+module.exports = resolvers;
