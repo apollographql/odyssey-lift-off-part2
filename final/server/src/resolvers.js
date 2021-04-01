d4e19e1ea48ed52b14abf14e7296fcaa6b265185
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-04-01 16:50:30 -0400

    add example for fetch

--- final/server/src/resolvers.js
+++ final/server/src/resolvers.js
@@ -1,13 +1,25 @@
+const fetch = require('node-fetch');
+
 const resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    tracksForHomeFetch: async () => {
+      const baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com';
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+    // using fetch instead of dataSources
+    author: async ({ authorId }, _, { dataSources }) => {
+      const baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com';
+      const res = await fetch(`${baseUrl}/author/${authorId}`);
+      return res.json();
+
+      // return dataSources.trackAPI.getAuthor(authorId);
     },
   },
 };
