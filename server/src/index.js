19af538d5347a2a39bfd62baa7b47541d7a3e907
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-07-16 09:17:16 -0600

    update server folders to as3

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
-const { ApolloServer, MockList } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => new MockList([6, 9]),
+    tracksForHome: () => [...new Array(9)],
   }),
   Track: () => ({
     id: () => 'track_01',
@@ -11,12 +11,10 @@ const mocks = {
     author: () => {
       return {
         name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
       };
     },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
     modulesCount: () => 6,
   }),
