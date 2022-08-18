fbe36c2c24c0bd206de7315ebd2b368d55f0db60
William Ferebee <williampferebee@gmail.com>
2022-08-18 02:34:40 -0400

    adding the resolvers now

---
+++ server/src/resolvers.js
@@ -0,0 +1,23 @@
+const resolvers = {
+    Query: {
+        // Returns an array of Tracks that will be used to populate
+        // - the homepage grid of our web client.
+        // The normal arguments are - parent, args, context, and info
+        // We replaced the first two with _, and __
+        // The last argument we are omitting since we dont need it and it comes
+        //  - after the argument that we do need - "context".
+        // With the "context" arg, we are destructuing it to
+        // - get the info we need {dataSources}
+        tracksForHome: (_, __, {dataSources}) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI
+        }
+    }
+};
+
+module.exports = resolvers;
