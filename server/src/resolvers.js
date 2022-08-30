1c484dcd540edb265522f1c81b7ba1b90749ad1c
Aidnac <calex6658@gmail.com>
2022-08-30 12:03:44 +0300

    The shape of resolvers

---
+++ server/src/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+  //returns an array of Tracks that will be used to populate
+  //the homepage grid of our web client
+  query: {
+    tracksForHome: (parent, args, context, info) => {},
+  },
+};
+
+module.exports = resolvers;
