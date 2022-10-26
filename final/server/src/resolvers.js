b7b52fddb1391988c7a37714754f9841d5da3c93
Megan Sullivan <meganesulli@gmail.com>
2022-10-26 15:47:58 -0700

    feat: add Query to get tracksForHome data using node-fetch instead of RESTDataSource

--- final/server/src/resolvers.js
+++ final/server/src/resolvers.js
@@ -1,13 +1,26 @@
+const fetch = require('node-fetch');
+
 const resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // returns the same data as tracksForHome, but uses node-fetch instead of RESTDataSource
+    tracksForHomeFetch: async () => {
+      const baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com';
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+    author: async ({ authorId }, _, { dataSources }) => {
+      // using fetch instead of dataSources
+      const baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com';
+      const res = await fetch(`${baseUrl}/author/${authorId}`);
+      return res.json();
+
+      // return dataSources.trackAPI.getAuthor(authorId);
     },
   },
 };
