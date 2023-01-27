f7d675a3fb9a04353c3b1818c32acb334c081303
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-01-26 21:19:03 -0700

    update to react router

--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,12 @@ const mocks = {
     author: () => {
       return {
         name: 'Grumpy Cat',
-        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+        photo:
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
       };
     },
-    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    thumbnail: () =>
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
     modulesCount: () => 6,
   }),
