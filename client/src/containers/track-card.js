50354d8845ca0c780148e469b2fe0406f2879823
lakshya104 <lakshya.vyas@anktech.co.in>
2024-02-16 12:24:15 +0530

    Mutation Completed

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,15 +3,35 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from "react-router-dom";
-/**
- * Track Card component renders basic info in a card format
- * for each track populating the tracks grid homepage.
- */
+import { gql, useMutation } from "@apollo/client"
+
+
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
+  
+  const INCREMENT_TRACK_VIEWS = gql`
+   mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      success
+      message
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+   `
+
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    onCompleted: (data) => {
+      console.log(data);
+    },
+  })
 
   return (
-    <CardContainer  to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
