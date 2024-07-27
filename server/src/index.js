f1f107bf7fcf216d8df7dfde78c2d82c588afa04
Rushikesh Patil (RISHI) <42404138+RRP12@users.noreply.github.com>
2024-07-27 12:39:48 +0000

    newschema added

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,38 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const { addMocksToSchema } = require('@graphql-tools/mock');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { resolvers, typeDefs } from './schema.js';
 
-const typeDefs = require('./schema');
+// const mocks = {
+//   Query: () => ({
+//     tracksForHome: () => [...new Array(6)],
+//   }),
+//   Track: () => ({
+//     id: () => 'track_01',
+//     title: () => 'Astro Kitty, Space Explorer',
+//     author: () => {
+//       return {
+//         name: 'Grumpy Cat',
+//         photo:
+//           'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+//       };
+//     },
+//     thumbnail: () =>
+//       'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+//     length: () => 1210,
+//     modulesCount: () => 6,
+//   }),
+// };
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+
+
 
 async function startApolloServer() {
   const server = new ApolloServer({
     schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
+      schema: makeExecutableSchema({ typeDefs , resolvers }),
+      // mocks,
     }),
   });
   const { url } = await startStandaloneServer(server);
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,10 +1,19 @@
-const gql = require('graphql-tag');
+import { gql }  from 'graphql-tag'
 
 const typeDefs = gql`
+
+
   type Query {
+    students : [Student!]
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
   }
+
+
+  type Student  {
+   name : String 
+   rollno : Int
+}
 
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
@@ -31,4 +40,27 @@ const typeDefs = gql`
   }
 `;
 
-module.exports = typeDefs;
+
+
+
+let data = [{
+  name : "rushikesh", 
+  rollno : 12
+}]
+
+const resolvers = {
+  Query: {
+    // returns an array of Tracks that will be used to populate
+    // the homepage grid of our web client
+    tracksForHome: () => {},
+    students : ( )  => [data]
+  }
+};
+
+
+
+
+export {
+  typeDefs ,
+  resolvers
+}
