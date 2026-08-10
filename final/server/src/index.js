792d3967157eb15ef381be8956342381b671701c
Michelle Mabuyo <michelle@apollographql.com>
2026-08-10 12:23:46 -0400

    Move server schema into schema.graphql and load it with readFileSync.
    Aligns Lift-off starters/finals with Odyssey lessons that define SDL in a .graphql file instead of embedding it in JS/TS via gql tags.

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -1,7 +1,15 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { readFileSync } = require('fs');
+const path = require('path');
+const gql = require('graphql-tag');
 
-const typeDefs = require('./schema');
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, './schema.graphql'), {
+    encoding: 'utf-8',
+  })
+);
+
 const resolvers = require('./resolvers');
 
 const TrackAPI = require('./datasources/track-api');
